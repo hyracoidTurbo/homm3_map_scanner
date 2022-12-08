daf021711699d27338f4d7e011cd2992e0f14f2e
hyracoidTurbo <50179107+hyracoidTurbo@users.noreply.github.com>
2022-12-08 10:59:09 +0100

    check if file is .png when joining screenshots

--- make_image_from_screenshots.py
+++ make_image_from_screenshots.py
@@ -19,31 +19,32 @@ mapa_ma_x_kratek = 252
 piksel_w_gornym_lewym_rogu = [15,138]
 
 for file_name in os.listdir("screenshots"):
-    img = Image.open(os.path.join("screenshots", file_name))
-    gorny_lewy_piksel = copy.copy(piksel_w_gornym_lewym_rogu)
-    x = int(file_name[0:2])
-    y = int(file_name[2:4])
+    if file_name.split(".")[-1] == "png":
+        img = Image.open(os.path.join("screenshots", file_name))
+        gorny_lewy_piksel = copy.copy(piksel_w_gornym_lewym_rogu)
+        x = int(file_name[0:2])
+        y = int(file_name[2:4])
+    
+        if y == 17:
+            gorny_lewy_piksel[1] += step * jedna_kratka_ma_x_piksele
 
-    if y == 17:
-        gorny_lewy_piksel[1] += step * jedna_kratka_ma_x_piksele
+        if x == 15:
+            gorny_lewy_piksel[0] += 8 * jedna_kratka_ma_x_piksele
+        if x == 16:
+            gorny_lewy_piksel[0] += (step + 8) * jedna_kratka_ma_x_piksele
+        if x == 17:
+            gorny_lewy_piksel[0] += (step + step + 8) * jedna_kratka_ma_x_piksele
 
-    if x == 15:
-        gorny_lewy_piksel[0] += 8 * jedna_kratka_ma_x_piksele
-    if x == 16:
-        gorny_lewy_piksel[0] += (step + 8) * jedna_kratka_ma_x_piksele
-    if x == 17:
-        gorny_lewy_piksel[0] += (step + step + 8) * jedna_kratka_ma_x_piksele
+        dolny_prawy_piksel = [gorny_lewy_piksel[0] + step * jedna_kratka_ma_x_piksele,
+                              gorny_lewy_piksel[1] + step * jedna_kratka_ma_x_piksele]
 
-    dolny_prawy_piksel = [gorny_lewy_piksel[0] + step * jedna_kratka_ma_x_piksele,
-                          gorny_lewy_piksel[1] + step * jedna_kratka_ma_x_piksele]
+        kafelek = img.crop([gorny_lewy_piksel[0],
+                            gorny_lewy_piksel[1],
+                            dolny_prawy_piksel[0],
+                            dolny_prawy_piksel[1]])
 
-    kafelek = img.crop([gorny_lewy_piksel[0],
-                        gorny_lewy_piksel[1],
-                        dolny_prawy_piksel[0],
-                        dolny_prawy_piksel[1]])
-
-    cala_mapa.paste(kafelek, (x * step * jedna_kratka_ma_x_piksele,
-                              y * step * jedna_kratka_ma_x_piksele))
+        cala_mapa.paste(kafelek, (x * step * jedna_kratka_ma_x_piksele,
+                                  y * step * jedna_kratka_ma_x_piksele))
 
 
 timestamp = time.time()
